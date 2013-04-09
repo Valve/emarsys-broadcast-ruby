8838d0cd545e07d2f9b0b370dd41d57bd4070a66
Valentin Vasilyev <iamvalentin@gmail.com>
2013-04-09 12:46:29 +0400

    Fixes

--- lib/emarsys/broadcast/batch.rb
+++ lib/emarsys/broadcast/batch.rb
@@ -14,11 +14,11 @@ module Emarsys
         :sender_domain,
         :sender_id
 
-        validates :name, :subject, :body_html, :recipients_path, :sender, :sender_domain, :sender_id, presence: true
+        validates :name, :subject, :body_html, :recipients_path, :sender, :sender_domain, presence: true
         validates :name, format: {with: /^[^\d\W]\w*\z/i, message: 'must start with a letter and contain only letters, numbers and underscores'}
         validates :subject, length: {maximum: 255}
         validates :sender, format: {with: /@/, message: 'is not a valid email'}
-        validates :sender_domain, format: {with: URI::regexp, message: 'is not valid'}
+        validates :sender_domain, format: {with: URI::REL_URI, message: 'is not valid'}
 
     end
   end
