c2345746664628c8ff190b01d30af821df6050b9
Krule <armin@pasalic.me>
2014-07-16 10:03:31 +0200

    Upgrade activemodel dependency to ~> 4.0

--- lib/emarsys/broadcast/batch.rb
+++ lib/emarsys/broadcast/batch.rb
@@ -4,7 +4,7 @@ module Emarsys
     class Batch
       include ActiveModel::Validations
       attr_accessor \
-        :name, 
+        :name,
         :subject,
         :body_html,
         :recipients_path,
@@ -16,11 +16,11 @@ module Emarsys
         :import_delay_hours
 
       validates :name, :subject, :body_html, :recipients_path, :sender, :sender_domain, presence: true
-      validates :name, format: {with: /^[^\d\W]\w*\z/i, message: 'must start with a letter and contain only letters, numbers and underscores'}
-      validates :subject, length: {maximum: 255}
-      validates :sender, format: {with: /@/, message: 'is not a valid email'}
-      validates :sender_domain, format: {with: URI::REL_URI, message: 'is not valid'}
-      
+      validates :name, format: { with: /\A[a-z]\w*\z/i, message: 'must start with a letter and contain only letters, numbers and underscores' }
+      validates :subject, length: { maximum: 255 }
+      validates :sender, format: { with: /@/, message: 'is not a valid email' }
+      validates :sender_domain, format: { with: URI::REL_URI, message: 'is not valid' }
+
       def initialize(attributes = {})
         attributes.each do |name, value|
           send("#{name}=", value)
